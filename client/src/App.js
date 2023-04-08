6595139a28fd052fd330a982448d78df8a107e53
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2023-04-08 16:51:25 -0300

    comenzando el front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Home from './components/Home/Home.jsx';
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
+      <Home/>
     </div>
   );
 }
